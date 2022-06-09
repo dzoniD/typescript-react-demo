b34dd81e1255f344575d3603a446b510a514fc52
dzoniD <edvard_dantes@hotmail.com>
2022-06-09 16:53:13 +0200

    discriminated unions in ts

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -5,8 +5,8 @@ type CounterState = {
 }
 
 type CounterAction = {
-    type: string
-    payload: number
+    type: 'increment' | 'decrement' | 'reset'
+    payload?: number
 }
 
 const initialState = { count: 0 }
@@ -17,6 +17,8 @@ function reducer(state: CounterState, action: CounterAction) {
             return { count: state.count + action.payload }
         case 'decrement' :
             return { count: state.count - action.payload }
+        case 'reset':
+            return initialState
         default: 
             return state
     }
@@ -29,6 +31,7 @@ export const Counter = () => {
             Count: {state.count}
             <button onClick={() => dispatch({ type: 'increment', payload:10 })}>Increment 10</button>
             <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>Decrement 10</button>
+            <button onClick={() => dispatch({ type: 'reset' })}> </button>
         </>
     )
 }
